cf7c0a88e5693e3be65a21335146fe30a290630d
aguss <agus.suprihadi@gmail.com>
2018-01-30 17:31:43 +0700

    swipe div

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,20 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
+import { HammerGestureConfig, HAMMER_GESTURE_CONFIG } from '@angular/platform-browser';
+
+export class MyHammerConfig extends HammerGestureConfig  {
+  overrides = <any>{
+      // override hammerjs default configuration
+      'pan': {threshold: 5},
+      'swipe': {
+           velocity: 0.4,
+           threshold: 20,
+           direction: 31 // /!\ ugly hack to allow swipe in all direction
+      }
+  }
+}
+
 @NgModule({
   declarations: [
     MyApp,
@@ -24,7 +38,9 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler}, 
+    {provide: HAMMER_GESTURE_CONFIG, useClass: MyHammerConfig }
+
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
@@ -6,9 +6,58 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  @ViewChild('div1') div1:ElementRef;
+  divClass:string='tumpuk1';
   constructor(public navCtrl: NavController) {
 
   }
-
+  divTap(){
+    //console.log(this.divClass)
+    if (this.divClass=='tumpuk1') {
+      //console.log('change')
+      this.divClass='tumpuk2'
+    } else if (this.divClass=='tumpuk2') {
+      //console.log('change')
+      this.divClass='tumpuk3'
+    } else if (this.divClass=='tumpuk3') {
+      //console.log('change')
+      this.divClass='tumpuk1'
+    }
+  }
+  download(event){
+    //console.log(event)
+    event.stopPropagation();
+    alert('download')
+  }
+  swipe(event) {
+    console.log('swipe ' + event.direction)
+    if(event.direction == 2) {
+    }
+    if(event.direction == 4) {
+    }
+    if(event.direction == 8) {
+      if (this.divClass=='tumpuk3') {
+        //console.log('change')
+        this.divClass='tumpuk2'
+      } else if (this.divClass=='tumpuk2') {
+        //console.log('change')
+        this.divClass='tumpuk1'
+      }
+    }
+    if(event.direction == 16) {
+      if (this.divClass=='tumpuk1') {
+        //console.log('change')
+        this.divClass='tumpuk2'
+      } else if (this.divClass=='tumpuk2') {
+        //console.log('change')
+        this.divClass='tumpuk3'
+      }
+    }
+  }
+  doRefresh(e){
+    console.log(e)
+  }
+  pan(e){
+    //console.log(e)
+  }
 }
